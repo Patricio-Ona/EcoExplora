c93ae539b493ff87044689775f64dd1d9d82e3e0
Patricio-Ona <pnona@puce.edu.ec>
2026-02-06 20:24:41 -0500

    Correcciones

--- src/app/[locale]/api/plantas.ts
+++ src/app/[locale]/api/plantas.ts
@@ -1,16 +1,25 @@
 import { PlantaSola } from '../types/types'
 import { Planta } from '../types/types'
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+if (!API_URL) {
+    throw new Error('NEXT_PUBLIC_API_URL no está definida en el .env.local')
+}
+
 export const getPlantas = async (): Promise<Planta[]> => {
     try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/plantas`, {
+        const response = await fetch(`${API_URL}/plantas`, {
             next: {
-                revalidate: 60, // Revalidar cada 60 segundos
+                revalidate: 60,
             },
         })
+
         if (!response.ok) {
+            console.error('STATUS ERROR:', response.status)
             throw new Error('Network response was not ok')
         }
+
         const data = await response.json()
         return data
     } catch (error) {
@@ -18,29 +27,34 @@ export const getPlantas = async (): Promise<Planta[]> => {
         throw error
     }
 }
+
 export const getPlanta = async (nombre: string) => {
-    const api = `${process.env.NEXT_PUBLIC_API_URL}/plantas/nombre/${encodeURIComponent(nombre)}`
+    const api = `${API_URL}/plantas/nombre/${encodeURIComponent(nombre)}`
     const response = await fetch(api)
+
     if (!response.ok) {
         console.error('Error fetching planta:', response.statusText)
         return null
     }
+
     const planta: PlantaSola = await response.json()
     return planta
 }
 
 export const postPlanta = async (planta: Planta): Promise<Planta> => {
     try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/plantas`, {
+        const response = await fetch(`${API_URL}/plantas`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(planta),
         })
+
         if (!response.ok) {
             throw new Error('Network response was not ok')
         }
+
         const data = await response.json()
         return data
     } catch (error) {
